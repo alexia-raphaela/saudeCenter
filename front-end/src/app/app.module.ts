a4e5e3aad7c5e61146acb29d8cbc3e5ccf2c413e
Alexia Raphaela da Silva <a.raphaela.da.silva@avanade.com>
2022-12-21 15:17:37 -0300

    conslusao do cadastro

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { CadastroHospitalComponent } from './cadastro-hospital/cadastro-hospital
 import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 import { CadastroEspecialidadeComponent } from './cadastro-especialidade/cadastro-especialidade.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/cadastro-paciente/cadastro.component.ts
+++ front-end/src/app/cadastro-paciente/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { IBeneficiarioDto } from '../interfaces/IBeneficiario';
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,7 +9,44 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
-  //@Input() beneficiario!: IBeneficiarioDto;
-  //@Input()  fecharCadastro!:() => void;
+  beneficiario!: IBeneficiarioDto;
+  idRecebido!: number;
+
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {
+    this.route.paramMap.subscribe(params => {
+      this.idRecebido = Number(params.get('id'));
+    });
+  }
+
+  //REST_API_SERVER = "https://localhost:7154/swagger/index.html";
+
+
+  ngOnInit(): void {
+    this.beneficiario = {
+      nome: '',
+      cpf: '',
+      email: '',
+      senha: '',
+      endereco: '',
+      telefone: '',
+      numeroCarteirinha: '',
+      ativo:true,
+    }
+  }
+
+  cadastrar() {
+    //implementar o objeto na api, chamar a api(subcribe)
+    this.http.post('https://localhost:7154/api/Beneficiarios', this.beneficiario)
+      .subscribe((data: any) => {
+          this.router.navigate(['beneficiario']);
+      });
+  }
+
+  submitFormularioTarefa(valorFormulario: any) {
+    console.log(`valor do formulário: ${JSON.stringify(valorFormulario)}`);
+    this.cadastrar();
+  }
 
 }
+
