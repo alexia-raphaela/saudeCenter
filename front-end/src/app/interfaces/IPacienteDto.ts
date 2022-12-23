698d51ab9915f3a65500045e241135821e2dcfd8
Alexia Raphaela da Silva <alexiaraphaela@gmail.com>
2022-12-22 23:23:59 -0300

    criado listagem de pacientes

---
+++ front-end/src/app/interfaces/IPacienteDto.ts
@@ -0,0 +1,9 @@
+export interface IPacienteDto {
+  nome:string,
+  cpf:string,
+  email:string,
+  endereco:string,
+  telefone:string,
+  numeroCarteirinha: string,
+  ativo:boolean,
+}
--- front-end/src/app/listagem-paciente/listagem-paciente.component.ts
+++ front-end/src/app/listagem-paciente/listagem-paciente.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
+import { IPacienteDto } from '../interfaces/IPacienteDto';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listagem-paciente',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class ListagemPacienteComponent {
 
+  listaPacientes: IPacienteDto[] = [];
+  telaParaApresentar = 'lista'
+
+  constructor(private http: HttpClient, private route: Router){
+    this.listarTodos();
+  }
+
+  listarTodos(){
+    this.http.get('https://localhost:7154/Beneficiario/ListarTodos')
+    .pipe(
+      map((response:any) => {
+        return Object.values(response);
+      })
+    )
+    .subscribe((data) => {
+      for(let index = 0; index <data.length; index++){
+        let contentJson:any = data[index];
+        this.listaPacientes.push(contentJson as IPacienteDto);
+      }
+    })
+  }
 }
